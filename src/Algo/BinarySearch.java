1ba5dee23f26edd4fc3dd436458586a949b85fe7
Roshan Kumar Das <droshankumar55@gmail.com>
2025-11-04 11:47:21 +0530

    celling and floor of a number

--- src/Algo/BinarySearch.java
+++ src/Algo/BinarySearch.java
@@ -66,6 +66,62 @@ public class BinarySearch {
         return -1;
     }
     //......................................................................
+    // Celling of a number
+    /*
+        find the smallest number in the array that is
+        greater than or equal to the target number (celling num >= target)
+     */
+    public static int cellingNumber(int[] arr, int target){
+        int start = 0;
+        int end = arr.length - 1;
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            if(target < arr[mid]){
+                end = mid - 1;
+            }
+            else if( target > arr[mid]){
+                start  = mid + 1;
+            }
+            else{
+                return mid;
+            }
+        }
+        if(start > arr.length - 1){
+            return -1;
+        }
+        // if the target element is greater than the last element in the array
+        return start;
+    }
+
+    // Floor of a number
+    /*
+        find the greatest number in the array that is
+        smaller than or equal to the target number (celling num <= target)
+     */
+
+    public static int floorNumber(int[] arr, int target){
+        int start = 0;
+        int end = arr.length - 1;
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            if(target < arr[mid]){
+                end = mid - 1;
+            }
+            else if( target > arr[mid]){
+                start  = mid + 1;
+            }
+            else{
+                return mid;
+            }
+        }
+        if(end < 0){
+            return -1;
+        }
+        // if the target element is the smaller than the 1st element in the array
+        return end;
+    }
+
+    //......................................................................
 
     public static void main(String[] args) {
         int[] sortedArray = {10, 20, 30, 40, 50, 60, 70, 80, 90};
@@ -74,5 +130,9 @@ public class BinarySearch {
         System.out.println("The target element is at index: " + index);
         int index2 = orderedBinarySearch(descArray,90);
         System.out.println("The target element is at index: " + index2);
+        int cellingIndex = cellingNumber(sortedArray, 55);
+        System.out.println("The celling number is at index: " + cellingIndex);
+        int floorIndex = floorNumber(sortedArray, 55);
+        System.out.println("The floor number is at index: " + floorIndex);
     }
 }
