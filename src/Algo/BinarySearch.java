e9a5e75a2d1e282778870c941c8d86086a88ff12
Roshan Kumar Das <droshankumar55@gmail.com>
2025-11-18 09:24:48 +0530

    binary search in matrix

--- src/Algo/BinarySearch.java
+++ src/Algo/BinarySearch.java
@@ -1,5 +1,7 @@
 package Algo;
 
+import java.util.Arrays;
+
 public class BinarySearch {
     // This algorithm is only applicable for sorted arrays.
     /*
@@ -159,11 +161,47 @@ public class BinarySearch {
         return binarySearchInRange(arr,target,start,end);
     }
     //......................................................................
+    /*
+    Let the matrix is sorted that is in both row and column manner
+
+    10 20 30 40
+    11 24 35 45
+    28 29 37 49
+    33 34 38 50
+
+    In matrix for a binary search we should focus on eliminating row and columns.
+
+    if the element is greater than the target then eliminate column.
+    if the element is less than the target then eliminate the row.
+     */
+
+    public static int[] binarySearchInMatrix(int[][] matrix, int target){
+        int row = 0;
+        int col = matrix.length - 1;
+        while ( (row < matrix.length) && (col >= 0) ){
+            if (matrix[row][col] == target){
+                return new int[]{row,col};
+            }
+            else if(matrix[row][col] > target){
+                col--;
+            }
+            else{
+                row++;
+            }
+        }
+        return new int[] {-1,-1};
+    }
+    //......................................................................
 
     public static void main(String[] args) {
         int[] sortedArray = {10, 20, 30, 40, 50, 60, 70, 80, 90};
         int[] descArray = {90,80,50,40,20,10};
         int[] longArray = {10,30,50,70,80,100,400,600,700,1000,30000,700000};
+
+        int[][] mat1 = {{10,20,30,40},{11,24,35,45},{28,29,37,49},{33,34,38,50}};
+        int[][] mat2 = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+
+
         int index = binarySearch(sortedArray, 40);
         System.out.println("The target element is at index: " + index);
         int index2 = orderedBinarySearch(descArray,90);
@@ -174,5 +212,7 @@ public class BinarySearch {
         System.out.println("The floor number is at index: " + floorIndex);
         int index3 = searchInInfiniteArray(longArray, 70);
         System.out.println("The target element is at index: " + index3);
+
+        System.out.println(Arrays.toString(binarySearchInMatrix(mat1,37)));
     }
 }
